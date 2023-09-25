ee1ef2dd0a5b6f510b841dcabbd787b1cce14ca1
Henrique Malvar <henriquemalvar@gmail.com>
2023-09-25 15:48:39 -0300

    fix: modify to show permission only

--- src/components/UserChip.js
+++ src/components/UserChip.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Chip } from "react-native-paper";
 import { permissionsIcons } from "../shared/util/constantsUtils";
+import { translatePermission } from "../shared/util/translationUtils";
 
 export default function UserChip({ parsedUser, onPress }) {
-    return (
-        <Chip
-            style={{
-                alignSelf: "flex-end",
-                marginRight: 10,
-                marginTop: 10,
-            }}
-            icon={permissionsIcons[parsedUser?.permission]}
-            mode="outlined"
-            onPress={onPress}
-        >
-            {parsedUser?.name}
-        </Chip>
-    );
+  return (
+    <Chip
+      style={{
+        alignSelf: "flex-end",
+        marginRight: 10,
+        marginTop: 10,
+      }}
+      icon={permissionsIcons[parsedUser?.permission]}
+      mode="outlined"
+      onPress={onPress}
+    >
+      {translatePermission(parsedUser?.permission)}
+    </Chip>
+  );
 }
