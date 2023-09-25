59d84bc7ed38c6bea9cfc57af205274a285bfbe8
Henrique Malvar <henriquemalvar@gmail.com>
2023-09-25 11:31:13 -0300

    refactor

--- src/shared/util/translationUtils.js
+++ src/shared/util/translationUtils.js
@@ -1,8 +1,9 @@
 import {
   permissions,
-  translatePermissions,
+  status,
   statusColor,
   statusIcon,
+  translatePermissions,
   translateStatusConstant,
 } from "./constantsUtils";
 
@@ -12,22 +13,38 @@ const _getColor = (status) => statusColor[status] || "black";
 
 const _getIcon = (status) => statusIcon[status] || "question";
 
-const _getOptions = (permission, currentStatus) => {
-  const options = permissions[permission] || [];
+const translatePermission = (permission) =>
+  translatePermissions[permission] || permission;
 
-  if (permission !== 'admin' && currentStatus) {
-    return options.filter((option) => option.from === currentStatus);
+const _getOptions = (permission, currentStatus) => {
+  let options = [];
+  if (permission === "admin") {
+    status.forEach((status) => {
+      options.push({ label: translateStatus(status), value: status });
+    });
+    options = options.filter((option) => option.value !== currentStatus);
+    return options;
   }
+
+  options = permission in permissions ? permissions[permission] : [];
+  if (currentStatus) {
+    options = options.filter((option) => option.from === currentStatus);
+  }
+
+  options = options.map((option) => {
+    return {
+      label: translateStatus(option.to || ""),
+      value: option.to || "",
+    };
+  });
 
   return options;
 };
 
-const translatePermission = (permission) => translatePermissions[permission] || permission;
-
 export {
-  translateStatus,
   _getColor,
   _getIcon,
   _getOptions,
   translatePermission,
+  translateStatus,
 };
