b1af034fd9693fe7f0779fbb707225e0632ab958
Henrique Malvar <henriquemalvar@gmail.com>
2023-11-12 17:54:53 -0300

    update components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,58 +1,73 @@
-import React from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import React, { useEffect, useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, Feather } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { parse } from "flatted";
 
-import Homepage from '../pages/home/Homepage';
-import QRCode from '../pages/qr-code/QRCode';
-import Perfil from '../pages/perfil/Perfil';
-
-import { Entypo, Feather } from '@expo/vector-icons';
+import Homepage from "../pages/home/Homepage";
+import QRCode from "../pages/qr-code/QRCode";
+import Perfil from "../pages/perfil/Perfil";
+import UserChip from "./UserChip";
 
 const Tab = createBottomTabNavigator();
 
 export default function Menu(props) {
+  const [parsedUser, setParsedUser] = useState(null);
 
-    const user = props.route.params.idUser;
+  useEffect(() => {
+    AsyncStorage.getItem("userConfig").then((user) => {
+      setParsedUser(parse(user));
+    });
+  }, []);
 
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarStyle: {
-                    paddingTop: 5,
-                    paddingBottom: 25,
-                    height: 70,
-                },
-            }
-            }>
-            <Tab.Screen
-                name="Leitos"
-                component={Homepage}
-                options={{
-                    tabBarIcon: ({ size, color }) => (
-                        <Entypo name="home" size={size} color={color} />
-                    )
-                }}
-            />
+  const user = props.route.params.idUser;
 
-            <Tab.Screen
-                name="QRCode"
-                component={QRCode}
-                options={{
-                    tabBarIcon: ({ size, color }) => (
-                        <Feather name="camera" size={size} color={color} />
-                    )
-                }}
-            />
+  return (
+    <>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            paddingTop: 5,
+            paddingBottom: 25,
+            height: 70,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Leitos"
+          component={Homepage}
+          options={{
+            headerRight: () => (
+              <UserChip parsedUser={parsedUser} onPress={() => {}} />
+            ),
+            tabBarLabel: "Leitos",
+            tabBarIcon: ({ size, color }) => (
+              <Entypo name="home" size={size} color={color} />
+            ),
+          }}
+        />
 
-            <Tab.Screen
-                name="Perfil"
-                component={Perfil}
-                initialParams={{ idUser: user }}
-                options={{
-                    tabBarIcon: ({ size, color }) => (
-                        <Entypo name="user" size={size} color={color} />
-                    )
-                }}
-            />
-        </Tab.Navigator>
-    );
+        <Tab.Screen
+          name="QRCode"
+          component={QRCode}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Feather name="camera" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Perfil"
+          component={Perfil}
+          initialParams={{ idUser: user }}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Entypo name="user" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </>
+  );
 }
