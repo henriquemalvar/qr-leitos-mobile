c3c5975117611779f40dfdb1652dca18ac9c3700
Henrique Malvar <henriquemalvar@gmail.com>
2024-03-26 01:18:52 -0300

    fix: add dialog to confirm update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { UpdateProvider } from "@contexts/UpdateProvider";
 import { NavigationContainer } from "@react-navigation/native";
 import { theme } from "@styles/theme";
 import { StatusBar } from "expo-status-bar";
@@ -7,22 +8,21 @@ import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import StackRoutes from "./StackRoutes";
-import { UpdateProvider } from "@contexts/UpdateProvider";
 
 export default function App() {
   SystemUI.setBackgroundColorAsync("#fff", true);
 
   return (
-    <UpdateProvider>
-      <SafeAreaProvider>
-        <PaperProvider theme={theme}>
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <UpdateProvider>
           <StatusBar style="auto" />
           <NavigationContainer>
             <StackRoutes />
           </NavigationContainer>
           <Toast />
-        </PaperProvider>
-      </SafeAreaProvider>
-    </UpdateProvider>
+        </UpdateProvider>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
--- src/shared/contexts/UpdateProvider.js
+++ src/shared/contexts/UpdateProvider.js
@@ -1,28 +1,34 @@
 import showMessage from "@utils/messageUtils";
 import Constants from "expo-constants";
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Linking } from "react-native";
+import { Button, Dialog, Paragraph } from "react-native-paper";
 
 export const UpdateContext = createContext();
 
 export const useUpdate = () => useContext(UpdateContext);
 
 export const UpdateProvider = ({ children }) => {
+  const [visible, setVisible] = useState(false);
+  const [updateLink, setUpdateLink] = useState(null);
+
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
 
   const compareVersions = (v1, v2) => {
-  const v1Parts = v1.split('.').map(Number);
-  const v2Parts = v2.split('.').map(Number);
+    const v1Parts = v1.split(".").map(Number);
+    const v2Parts = v2.split(".").map(Number);
 
-  for (let i = 0; i < v1Parts.length; i++) {
-    if (v1Parts[i] > v2Parts[i]) {
-      return 1;
-    } else if (v1Parts[i] < v2Parts[i]) {
-      return -1;
+    for (let i = 0; i < v1Parts.length; i++) {
+      if (v1Parts[i] > v2Parts[i]) {
+        return 1;
+      } else if (v1Parts[i] < v2Parts[i]) {
+        return -1;
+      }
     }
-  }
 
-  return 0;
-}
+    return 0;
+  };
   const fetchUpdateLink = async () => {
     const response = await fetch(
       "https://qr-leitos.vercel.app/api/app/check-update"
@@ -35,13 +41,14 @@ export const UpdateProvider = ({ children }) => {
     const currentVersion = Constants.expoConfig.version;
 
     if (version && compareVersions(version, currentVersion) > 0) {
-      showMessage(
-        "info",
-        "Atualizações",
-        "Há uma atualização disponível. Abrindo link..."
-      );
-      Linking.openURL(link);
+      setUpdateLink(link);
+      showDialog();
     }
+  };
+
+  const handleUpdate = () => {
+    hideDialog();
+    Linking.openURL(updateLink);
   };
 
   const fetchUpdates = async () => {
@@ -79,10 +86,22 @@ export const UpdateProvider = ({ children }) => {
         fetchUpdateLink,
         handleNewUpdateLink,
         handleError,
-        compareVersions
+        compareVersions,
       }}
     >
       {children}
+      <Dialog visible={visible} onDismiss={hideDialog}>
+        <Dialog.Title>Atualizações</Dialog.Title>
+        <Dialog.Content>
+          <Paragraph>
+            Há uma atualização disponível. Deseja atualizar agora?
+          </Paragraph>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={hideDialog}>Cancelar</Button>
+          <Button onPress={handleUpdate}>Atualizar</Button>
+        </Dialog.Actions>
+      </Dialog>
     </UpdateContext.Provider>
   );
 };
